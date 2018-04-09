d016e771edfac6f3348de95753f734cfdedb7aee
sesstigit <sesstigit@gmail.com>
2018-04-09 17:56:10 +0930

    adjusted code to cope with one motor and one steering servo, rather than motors on two wheels.  Also adjust for extra IR sensor (6 sensors rather than 5).

--- ArduinoLineFollower/robotDefines.h
+++ ArduinoLineFollower/robotDefines.h
@@ -4,6 +4,20 @@ int mode = 0;
 # define FOLLOWING_LINE 1
 # define NO_LINE 2
 
+// From AndroidCar
+/* pin definitions */
+#define SERVO_PIN 8
+#define ESC_PIN 9
+#define LEFT_IR_ARRAY_L A8
+#define LEFT_IR_ARRAY_R A9
+#define MID_IR_ARRAY_L A6
+#define MID_IR_ARRAY_R A7
+#define RIGHT_IR_ARRAY_L A10
+#define RIGHT_IR_ARRAY_R A11
+#define WHITE 1
+#define BLACK 0
+// End from AndroidCar
+
 const int power = 500;
 const int iniMotorPower = 250;
 const int adj = 1;
@@ -13,13 +27,14 @@ const int ledPin = 13;
 const int buttonPin = 9;
 
 // LFSensor more to the Left is "0"
-const int lineFollowSensor0 = 12; 
-const int lineFollowSensor1 = 18; 
-const int lineFollowSensor2 = 17; 
-const int lineFollowSensor3 = 16;
-const int lineFollowSensor4 = 19;
+const int lineFollowSensor0 = LEFT_IR_ARRAY_L; 
+const int lineFollowSensor1 = LEFT_IR_ARRAY_R; 
+const int lineFollowSensor2 = MID_IR_ARRAY_L; 
+const int lineFollowSensor3 = MID_IR_ARRAY_R;
+const int lineFollowSensor4 = RIGHT_IR_ARRAY_L;
+const int lineFollowSensor5 = RIGHT_IR_ARRAY_R;
 
-int LFSensor[5]={0, 0, 0, 0, 0};
+int LFSensor[6]={0, 0, 0, 0, 0, 0};
 
 // PID controller
 float Kp=50;
@@ -29,10 +44,20 @@ float Kd=0;
 float error=0, P=0, I=0, D=0, PIDvalue=0;
 float previousError=0, previousI=0;
 
-#define RIGHT 1
-#define LEFT -1
+#define RIGHT -1
+#define LEFT 1
 
-Servo leftServo;
-Servo rightServo;
+# Motor and steering constants
+const int IDLE_SPEED = 1500;
+const int MAX_FRONT_SPEED = 2000;
+const int MAX_BACK_SPEED = 1000;
+const int STRAIGHT_WHEELS = 90;
+const int MAX_RIGHT_DEGREES = 40;
+const int MAX_LEFT_DEGREES = 140;
+#define OVERRIDE_FORWARD_SPEED 120
+#define OVERRIDE_BACKWARD_SPEED -20
+#define OVERRIDE_STEER_RIGHT 20
+#define OVERRIDE_STEER_LEFT -20
 
-
+Servo esc;  //ESC for the motor
+Servo steer;  //Servo for steering
